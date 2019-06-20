d67d4554bbd5db50f11ef1dc6ef04245232d0659
MaJlBapb <48066751+MaJlBapb@users.noreply.github.com>
2019-06-20 16:18:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-(function($) {
-	var target = document.querySelector('.email');
-
-	function selection(elem) {
-		var elem   = document.querySelector(elem);
-		var select = window.getSelection();
-		var range  = document.createRange();
-
-		range.selectNodeContents(elem);
-		select.addRange(range);
-	}
-	
-	target.onclick = function() {
-		selection('.email');
-	};
-})(jQuery);
+function selectText(elementId) {
+var doc = document,
+text = doc.getElementById(elementId),
+range,
+selection;
+if (doc.body.createTextRange) {
+range = document.body.createTextRange();
+range.moveToElementText(text);
+range.select();
+} else if (window.getSelection) {
+selection = window.getSelection();
+range = document.createRange();
+range.selectNodeContents(text);
+selection.removeAllRanges();
+selection.addRange(range);
+}}
+$(".email").click(function() {
+selectText(this.id);
+document.execCommand("copy");
+});
