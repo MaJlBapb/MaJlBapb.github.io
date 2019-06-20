522230570c95dad7fdaa87cf8fa0d1bb739c0152
MaJlBapb <48066751+MaJlBapb@users.noreply.github.com>
2019-06-20 16:23:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-function selectText(elementId) {
-var doc = document,
-text = doc.getElementById(elementId),
-range,
-selection;
-if (doc.body.createTextRange) {
-range = document.body.createTextRange();
-range.moveToElementText(text);
-range.select();
-} else if (window.getSelection) {
-selection = window.getSelection();
-range = document.createRange();
-range.selectNodeContents(text);
-selection.removeAllRanges();
-selection.addRange(range);
-}}
-$(".email").click(function() {
-selectText(this.id);
-document.execCommand("copy");
-});
+function clipboard() {
+  var copyText = document.getElementById("myInput");
+  copyText.select();
+  document.execCommand("copy");
+}
